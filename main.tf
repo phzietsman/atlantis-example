fe37092a778a625aa9d1f689e4f8d490e3149a6a
Paul Zietsman <phzietsman@gmail.com>
2021-03-12 16:07:19 +0200

    AWS resources

--- main.tf
+++ main.tf
@@ -1,7 +1,27 @@
-resource "null_resource" "example" {}
+terraform {
 
-resource "null_resource" "example2" {}
+    // Atlantis will download this
+    required_version = "0.12.26"
 
-output "workspace" {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_ssm_parameter" "atlantis" {
+  name  = "atlantis-${var.env}"
+  type  = "String"
   value = terraform.workspace
+}
+
+variable "env" {
+    type = string
+    default = "default"
 }
